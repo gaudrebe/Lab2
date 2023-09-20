95925cc675a97199cc99073ce080933f6ac52db2
joseph rumery <joseph.rumery@maine.edu>
2023-09-20 14:41:24 -0400

    changes

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,3 +1,38 @@
 public class MyArrayList implements ArrayList<Fraction> {
 
+	public void add(Fraction frc) {
+		
+	}
+	
+	public boolean hasNext() {
+		
+	}
+	
+	public boolean hasPrevious() {
+		
+	}
+	
+	public Fraction next() {
+		
+	}
+	
+	public int nextIndex() {
+		
+	}
+	
+	public Fraction previous() {
+		
+	}
+	
+	public int previousIndex() {
+		
+	}
+	
+	public void remove() {
+		
+	}
+	
+	public void set(Fraction frc) {
+		
+	}
 }
